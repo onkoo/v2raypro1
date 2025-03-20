6b68d3d0c7403b5d83e45b81e0ec8eab82bdbb98
onkoo <on6@qq.com>
2025-03-20 20:59:00 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -10,7 +10,7 @@ import { connect } from 'cloudflare:sockets';
  * User configuration and settings
  * Generate UUID: [Windows] Press "Win + R", input cmd and run: Powershell -NoExit -Command "[guid]::NewGuid()"
  */
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'ba034b1b-4835-4be3-b21d-6126a3ec8c51';
 
 /**
  * Array of proxy server addresses with ports
